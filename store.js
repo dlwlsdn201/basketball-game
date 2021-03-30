7d1243e57cc16b4cd876d11031191f312454ba79
LeeJinWoo <dlwlsdn201@naver.com>
2021-03-30 14:47:44 +0900

    [fix]시작 전 슛버튼 비활성화 기능 수정 완료_ closed #25

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
-import rootReducer from  './modules/index';
-import {composeWithDevTools} from 'redux-devtools-extension';
+// import { createStore } from 'redux';
+// import rootReducer from  './modules/index';
+// import {composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
 
-export default store;
+// export default store;
